5a2849de6957b9609c125ace469e0989190a5f22
hjbuchanan <hannahbuchanan4@gmail.com>
2014-05-01 14:25:09 -0400

    secure version

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -22,73 +22,18 @@ class BooksController < ApplicationController
   # GET /books/new
   def new
     @book = Book.new
-    # Amazon::Ecs.options = {
-    #     :associate_tag => '427806390514',
-    #     :AWS_access_key_id => 'AKIAI4CA4E7EODE7CRWA',
-    #     :AWS_secret_key => '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'
-    #   }
-    #   # res = Amazon::Ecs.item_lookup('9780521153348', :response_group => 'ItemAttributes', :id_type => 'ISBN', :search_index => 'Books')
-    #   #   res.each do |item|
-    #   #     item_attributes = item.get_element('ItemAttributes')
-    #   #     item_attributes.get('Title')
-    #   #   end
-    #   # @isbn = params[:book][:isbn]
-    #   @book[:ISBN]
-    #   @isbnn = @book[:ISBN]
-    #   search_term = params[:search] || "#{@isbn}"
-    #   @res  = Amazon::Ecs.item_lookup(search_term,  { :search_index => 'Books', :id_type => "ISBN", :response_group => "ItemAttributes"})
-    #   @imgs = Amazon::Ecs.item_lookup(search_term,  { :search_index => 'Books', :id_type => "ISBN", :response_group => 'Images',
-    #                                                   :search_index => 'Books',
-    #                                                   :sort => 'relevancerank' })
 
-    #   # need to save author, title, publisher of first returned item
-    #   @new_book = @res.items.first
-    #     @book.title = @res.items.first.get_element('ItemAttributes').get('Title')
-    #     @book.author = @res.items.first.get_element('ItemAttributes').get('Author')
-    #     @book.publisher = @res.items.first.get_element('ItemAttributes').get('Publisher')
-    #     @book.save
 
 
   end
 
-  # def lookup
-  #    Amazon::Ecs.options = {
-  #       :associate_tag => '427806390514',
-  #       :AWS_access_key_id => 'AKIAI4CA4E7EODE7CRWA',
-  #       :AWS_secret_key => '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'
-  #     }
-  #     # res = Amazon::Ecs.item_lookup('9780521153348', :response_group => 'ItemAttributes', :id_type => 'ISBN', :search_index => 'Books')
-  #     #   res.each do |item|
-  #     #     item_attributes = item.get_element('ItemAttributes')
-  #     #     item_attributes.get('Title')
-  #     #   end
-  #     # @isbn = params[:book][:isbn]
-  #     @book[:ISBN]
-  #     @isbn = @book[:ISBN]
-  #     search_term = params[:search] || "#{@isbn}"
-  #     @res  = Amazon::Ecs.item_lookup(search_term,  { :search_index => 'Books', :id_type => "ISBN", :response_group => "ItemAttributes"})
-  #     @imgs = Amazon::Ecs.item_lookup(search_term,  { :search_index => 'Books', :id_type => "ISBN", :response_group => 'Images',
-  #                                                     :search_index => 'Books',
-  #                                                     :sort => 'relevancerank' })
 
-  #     # need to save author, title, publisher of first returned item
-  #     @new_book = @res.items.first
-  #       @book.title = @res.items.first.get_element('ItemAttributes').get('Title')
-  #       @book.author = @res.items.first.get_element('ItemAttributes').get('Author')
-  #       @book.publisher = @res.items.first.get_element('ItemAttributes').get('Publisher')
-  #       @book.save
-  # end
 
 
   # GET /books/1/edit
   def edit
   end
 
-  # def get_book_info
-  #   @response = HTTPpart.get('http://webservices.amazon.com/onca/xml?Service=AWSECommerceService&Operation=ItemLookup&SubscriptionId=AKIAI6MPLS2WWOQTVEIQ&AssociateTag=286193100284&Version=2011-08-01&ItemId=9788478888566&IdType=ISBN&SearchIndex=Books&Condition=All&ResponseGroup=Images,ItemAttributes,Offers')
-  #   data = @response.parsed_response
-  #   puts data['title']
-  # end
 
 
   # POST /books
@@ -105,11 +50,7 @@ class BooksController < ApplicationController
     #     format.json { render json: @book.errors, status: :unprocessable_entity }
     #   end
     # end
-    Amazon::Ecs.options = {
-        :associate_tag => '427806390514',
-        :AWS_access_key_id => 'AKIAI4CA4E7EODE7CRWA',
-        :AWS_secret_key => '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'
-      }
+
 
 
       @isbn = params[:book][:ISBN]
